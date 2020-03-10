332042bf535f6c5100a68da288d7fe27e8337e11
fredlc <fredlefevre@outlook.com>
2020-03-10 19:44:47 -0400

    Built backtracking algorithm to solve sudoku board

--- solver.py
+++ solver.py
@@ -10,6 +10,48 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if is_valid(board, i, (row,col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+def is_valid(board, num, position):
+
+    # Check row
+    for i in range(len(board[0])):
+        if board[position[0]][i] == num and position[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(board)):
+        if board[i][position[1]] == num and position[0] != i:
+            return False
+
+    # Check box
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i,j) != position:
+                return False
+
+    return True
+
 def print_board(board):
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
@@ -33,4 +75,7 @@ def find_empty(board):
 
     return None
 
+print_board(board)
+solve(board)
+print('________________________________')
 print_board(board)
